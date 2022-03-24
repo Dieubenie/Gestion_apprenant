356c74c52e5a730819eb356093cfcdc0ccb68e7c
Dieubénie <kodjim15@gmail.com>
2022-03-24 11:26:32 +0100

    Transformation des fichiers en php

---
+++ Projet_apprenant/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include 'pages/style.php';?>
+</head>
+<body>
+<?php include 'pages/menu.php';?>  
+
+      <h1 style="font-family: Algerian; text-align: center;"> SOYEZ LES BIENVENUS A LA GESTIONS DES APPRENANTS</h1>
+      <div class="row">
+        <img src="image/etudiant.jpg" width="100%" height="430px" alt="">
+      </div> <br>
+      <?php include 'pages/pied.php';?>
+      <?php include 'pages/script.php';?>
+</body>
+</html>
---
+++ Projet_apprenant/pages/formulaire_apprenant.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include 'style.php';?>
+</head>
+<body>
+<?php include 'pages/menu.php';?>  
+<h1 style="font-family: Castellar;"> INSEREZ UN APPRENANT</h1>
+<div class="d-grid gap-2 d-md-flex justify-content-md-end">
+  <button class="btn btn-primary me-md-2" type="button">Button</button>
+  <button class="btn btn-primary" type="button">Button</button>
+</div>
+      <br>
+      <?php include 'pied.php';?>
+      <?php include 'script.php';?>
+</body>
+</html>
